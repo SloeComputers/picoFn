014a7ebbd9d7839615423e0ee720801715e33527
John D. Haughton <sloecomputer@gmail.com>
2026-08-23 19:10:56 +0100

    Plugin build

---
+++ Source/picofn_plugin.cpp
@@ -0,0 +1,52 @@
+//-------------------------------------------------------------------------------
+// Copyright (c) 2026 John D. Haughton
+// SPDX-License-Identifier: MIT
+//-------------------------------------------------------------------------------
+
+#undef RELEASE
+
+#include "Generator.h"
+#include "SIG/ReSample.h"
+
+class PluginSynth : public Generator
+{
+public:
+   PluginSynth() = default;
+
+   //! Get generator rate sample
+   SIG::Signal operator()()
+   {
+      constexpr unsigned SAMPLES_PER_TICK = DX7::SAMPLE_RATE / DX7::TICK_RATE;
+
+      if (--samples_to_tick < 0)
+      {
+         samples_to_tick = SAMPLES_PER_TICK - 1;
+         tick();
+      }
+
+      int32_t value = getSampleSingle();
+
+      return ((value << 16) >> 16) / 32768.0;
+   }
+
+   void setSampleRate(unsigned sample_rate_) override
+   {
+      re_sample.setOutRate(sample_rate_);
+   }
+
+private:
+   //! Get DAW rate sample
+   SIG::Signal sample() override
+   {
+      return re_sample();
+   }
+
+   signed samples_to_tick{0};
+
+   SIG::ReSample<PluginSynth,/* N */ 16> re_sample{*this, DX7::SAMPLE_RATE};
+};
+
+MIDI::Synth* MIDI::Synth::construct()
+{
+   return new PluginSynth();
+}
